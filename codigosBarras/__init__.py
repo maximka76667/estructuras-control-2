6deb8513d008a3df69d9ed22baa234ed2b34b3a9
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-11-09 14:04:09 +0100

    codigo de barras 1

--- codigosBarras/__init__.py
+++ codigosBarras/__init__.py
@@ -1,5 +1,22 @@
+from _functools import reduce
+from math import floor
 codigo = input("Codigo: ")
+# codigo = "67839520"
+# codigo = "65839522"
 
-for i in range(codigo.__len__()-2, -1, -1):
-    print(codigo[i])
+controlErrores = []
 
+for i in range(len(codigo)-2, -1, -1):
+    if((i + 1) % 2 == 0):
+        controlErrores.append(int(codigo[i]))
+        continue
+    controlErrores.append(int(codigo[i]) * 3)
+    
+suma = reduce(lambda a, b: a + b, controlErrores)   
+
+digitoControl = 0 if suma % 10 == 0 else (floor(suma / 10) + 1) * 10 - suma
+
+if digitoControl == int(codigo[-1]):
+    print("SI")
+else:
+    print("NO")
